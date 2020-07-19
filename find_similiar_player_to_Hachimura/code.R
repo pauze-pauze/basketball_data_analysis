36baf79677f8af2e70c9ebed05a557295e2ec46a
pauze-pauze <yhaimsaaztaakkia1160@gmail.com>
2020-07-19 11:14:39 +0900

    update code

--- find_similiar_player_to_Hachimura/code.R
+++ find_similiar_player_to_Hachimura/code.R
@@ -3,16 +3,19 @@
 
 library(tidyverse)
 library(ballr)
-
-
-
+library(cluster)
+library(skimr)
+library(ggrepel)
+library(factoextra)
 
 # 利用するデータの作成 --------------------------------------------------------------
 
 #データ取得_36分換算データ
-base <- NBAPerGameStatisticsPer36Min(season = 2010) %>%
-  mutate(season = 2010)
-for (i in 2011:2020){
+base_season = 1998
+
+base <- NBAPerGameStatisticsPer36Min(season = base_season) %>%
+  mutate(season = base_season)
+for (i in base_season + 1:2020){
   bind <- NBAPerGameStatisticsPer36Min(season = i) %>%
     mutate(season = i)
   base <- base %>%
@@ -20,10 +23,10 @@ for (i in 2011:2020){
 }
 
 #データ取得_advanced_statistics
-base2 <- NBAPerGameAdvStatistics(season = 2010) %>%
-  mutate(season = 2010)
+base2 <- NBAPerGameAdvStatistics(season = base_season) %>%
+  mutate(season = base_season)
 
-for (i in 2011:2020){
+for (i in base_season + 1:2020){
   bind <- NBAPerGameAdvStatistics(season = i) %>%
     mutate(season = i)
   base2 <- base2 %>%
@@ -42,7 +45,7 @@ tot_flag <- base %>%
     )
   )
 
-data <- base %>%
+data_base <- base %>%
   inner_join(
     base2
     ,by = c(
@@ -65,4 +68,257 @@ data <- base %>%
   ) %>%
   filter(
     xor(tm != "TOT", tot_flag == 1) # 同一シーズンで複数チームに所属していた選手をシーズン全体のスタッツだけ残す
+  ) %>%
+  select(
+    -c(
+      rk.x
+      ,tm
+      ,link
+      ,rk.y
+      ,x
+      ,x_2
+    )
+  ) %>%
+  mutate(
+    mpg = mp/g
+    ,a2to = ast / tov
   )
+
+# 2020シーズンにプレイした選手とそのデビューシーズンの抽出
+player_20 <- data_base %>%
+  filter(
+    season == 2020
+  ) %>%
+  select(
+    player
+    ,season
+  ) %>%
+  distinct()
+
+player_first_season <- data_base %>%
+  group_by(
+    player
+  ) %>%
+  summarise(
+    season = min(season)
+  ) %>%
+  inner_join(
+    player_20
+    ,by = "player"
+  ) %>%
+  mutate(
+    season = season.x
+  ) %>%
+  select(
+    player
+    ,season
+  ) 
+player_list <- player_20 %>%
+  bind_rows(player_first_season) %>%
+  distinct()
+
+data <- data_base %>%
+  inner_join(
+    player_list
+    ,by = c("player", "season")
+  )
+# 出場時間の分布を見て、下限を決める(八村が残るように)
+data %>%
+  ggplot(aes(x = mp))+
+  geom_histogram()
+data %>% # 八村の出場時間を確認
+  filter(
+    player == "Rui Hachimura"
+  ) %>%
+  select(
+    mp
+  )
+#2020シーズンに2000分以上プレイした選手と八村のリスト作成
+data_mp_20 <- data %>%
+  filter(
+    season == 2020
+  ) %>%
+  filter(
+    mp >= 1750 #ヤニス入れたいがゆえの時間設定
+    | player == "Rui Hachimura"
+  ) %>%
+  select(
+    player
+  )
+data_mp_filt <- data %>%
+  inner_join(
+    data_mp_20
+    ,by = "player"
+  ) %>%
+  na.omit()
+  
+#
+#2020シーズンは途中までしか試合がないから、MPやG、GSを変数に加えると外的要因が混じりこむので、基本的には割合ベースの数値にする
+#
+# 正規化
+
+data_main <- data_mp_filt %>%
+  mutate(
+    player = str_c(player, as.character(season), sep = "_")
+  ) %>%
+  select(
+    -c(
+      g
+      ,gs
+      ,mp
+      ,pos
+      ,age
+      ,season
+    )
+  ) %>%
+  mutate(
+    x3ppercent = if_else(is.na(x3ppercent) == TRUE, 0, x3ppercent)
+  ) %>%
+  mutate_at(vars(-c(player)), funs(scale)) 
+
+rownames(data_main) <- data_main$player
+
+skim(data_main)
+
+
+# 多次元尺度法 ------------------------------------------------------------------
+
+res_dist <- data_main %>%
+  select(-player) %>%
+  dist()
+data_cmd <- cmdscale(res_dist)
+plot(data_cmd)
+data_lab <- data_main$player
+text(data_cmd, labels = data_lab)
+
+# 主成分分析 -------------------------------------------------------------------
+
+res_cov <- data_main %>%
+  select(
+    -player
+  ) %>%
+  cov()
+
+round(res_cov, 2)
+
+eig <- eigen(res_cov)$vectors
+
+eigenv2 <- eig[,(1:2)]
+statsMatrix <- data_main %>%
+  select(
+    -player
+  ) %>%
+  data.matrix(rownames.force = NA)
+PrincipalComponents2 <- statsMatrix %*% eigenv2
+
+statsDF <- data.frame(PrincipalComponents2) %>%
+  bind_cols(data_main[1]) %>%
+  mutate(
+    season_20_flag = case_when(
+      str_sub(player, start = -4, end = -1) == "2020" ~ "2020"
+      ,TRUE ~ "デビュー"
+    )
+  )
+
+colors2=c("black","gray50")
+
+ggplot(statsDF, aes(statsDF$X1, -statsDF$X2, fill = season_20_flag, color = season_20_flag))+ #x2をマイナスにしたのはk-meansと向きを合わせるため
+  geom_point()+
+  geom_text_repel(aes(label = player), size = 3)+
+  scale_color_manual(values = colors2)+
+  theme_classic()
+
+
+#距離の近い選手をリスト化
+hachi_x1 <- statsDF %>%
+  filter(
+    player == "Rui Hachimura_2020"
+  ) %>%
+  select(
+    X1
+  ) %>%
+  as.numeric()
+hachi_x2 <- statsDF %>%
+  filter(
+    player == "Rui Hachimura_2020"
+  ) %>%
+  select(
+    X2
+  ) %>%
+  as.numeric()
+dist_rank <-  statsDF %>%
+  mutate(
+    X1_dist = X1 - hachi_x1
+    ,X2_dist = X2 - hachi_x2
+    ,dist = sqrt(X1_dist^2 + X2_dist^2)
+  ) %>%
+  arrange(
+    dist
+  )
+# 階層的クラスタリング --------------------------------------------------------------
+rownames(data_main) <- data_main$player
+dist_hclust <- data_main %>%
+  select(
+    -player
+  ) %>%
+  dist()
+data_hclust <- hclust(dist_hclust)
+plot(data_hclust)
+fviz_dend(
+  data_hclust
+  #,k = 8
+  ,cex = 0.4
+  ,horiz = TRUE
+  ,rect = TRUE
+  ,labelsize = 1
+)
+
+data_hclust
+# 距離の計算方法はすべてユークリッド距離
+# k-means -----------------------------------------------------------------
+
+res_dist <- data_main %>%
+  select(
+    -player
+  ) %>%
+  get_dist(
+    stand = TRUE
+    ,method = "euclidean"
+  )
+data_main %>%
+  select(
+    -player
+  ) %>%
+  fviz_nbclust(kmeans, method = "wss")
+
+data_main %>%
+  select(
+    -player
+  ) %>%
+  fviz_nbclust(kmeans, method = "silhouette")
+
+data_main %>%
+  select(
+    -player
+  ) %>%
+  fviz_nbclust(kmeans, method = "gap_stat")
+
+km_res <- data_main %>%
+  select(
+    -player
+  ) %>%
+  kmeans(10, nstart = 100)
+
+
+data_main_kmeans <- data_main %>%
+  select(
+    -player
+  )
+
+fviz_cluster(km_res, data_main_kmeans, ellipse = TRUE, ellipse.alpha= 0.1,
+             palette = "jco",repel = TRUE, ggtheme = theme_classic(), 
+             main= FALSE, xlab= FALSE, ylab = FALSE,
+             labelsize= 8,lwd=2
+)
+
+
