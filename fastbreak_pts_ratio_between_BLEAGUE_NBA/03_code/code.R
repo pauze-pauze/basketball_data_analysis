59553831e4b09acd6e5a0897ed505b06e680a45b
pauze-pauze <yhaimsaaztaakkia1160@gmail.com>
2021-06-23 22:25:05 +0900

    update fast break ratio data

--- fastbreak_pts_ratio_between_BLEAGUE_NBA/03_code/code.R
+++ fastbreak_pts_ratio_between_BLEAGUE_NBA/03_code/code.R
@@ -2,6 +2,7 @@
 # import libraries --------------------------------------------------------
 
 library(tidyverse)
+library(scales)
 library(lubridate)
 library(skimr)
 library(ggrepel)
@@ -10,8 +11,6 @@ library(patchwork)
 
 
 # B.LEAGUE data -----------------------------------------------------------
-
-
 
 # _load data ---------------------------------------------------------------
 
@@ -28,7 +27,6 @@ skim(team)
 
 # pick up season and division
 
-season_param = c("2017-18", "2018-19", "2020-21") # コロナで中断したシーズンを除外
 division_param = 2 # B1のレギュラーシーズンに絞る
 
 
@@ -43,7 +41,6 @@ data_base_bleague <- game_summary %>%
     ,by = c("TeamId", "Season")
   )%>%
   filter(
-    #Season %in% season_param
     EventId == division_param
   ) 
 
@@ -63,8 +60,103 @@ output_bleague <- data_base_bleague %>%
 
 write.csv(output_bleague, "./02_output/bleague_data.csv", fileEncoding = "CP932", row.names = FALSE)
 
-output_bleague %>%
-  ggplot(aes(x = fast_break_ratio)) +
-  geom_histogram()
 
-write.csv()
+
+
+
+# NBA_data ----------------------------------------------------------------
+
+
+# _loda data --------------------------------------------------------------
+
+library(nbastatR)
+
+?teams_players_stats
+
+t <- teams_players_stats(
+  seasons = c(2017:2021)
+  ,types = "team"
+  ,tables = "general"
+  ,measures = "Scoring"
+  ,modes = "Totals"
+)
+tt <- unnest(t, cols = dataTable)
+
+output_NBA <- tt %>%
+  select(
+    slugSeason
+    ,nameTeam
+    ,pctPTSasFB
+  ) %>%
+  mutate(
+    fast_break_ratio = pctPTSasFB * 4 / 3 # 定義の違いを調整するための仮定
+  ) %>%
+  select(
+    slugSeason
+    ,nameTeam
+    ,fast_break_ratio
+  ) %>%
+  rename(
+    Season = slugSeason
+    ,Team = nameTeam
+  ) %>%
+  mutate(
+    league = "NBA"
+  )
+
+
+# ヒストグラムで分布の比較と順位のアウトプット ------------------------------------------------------------
+
+
+hist_base <- output_bleague %>%
+  select(
+    Season
+    ,NameLong
+    ,fast_break_ratio
+  ) %>%
+  rename(
+    Team = NameLong
+  ) %>%
+  mutate(
+    league = "B.LEAGUE"
+  ) %>%
+  rbind(output_NBA)
+  
+hist_base %>%
+  group_by(league) %>%
+  summarise(cnt = n())
+
+p <- ggplot(data = hist_base, aes(x = fast_break_ratio))+
+  geom_histogram(aes(color = league, fill = league), alpha = 0.5) +
+  labs(x = "得点に占めるファストブレイクの割合"
+       ,y = "該当チーム数"
+       ,title = "B.LEAGUEとNBAのファストブレイク構成比の比較"
+       ,subtitle = "  2016-17~2020-21のB1とNBAのレギュラーシーズンが対象"
+       ,caption = "NBAとFIBAでのファストブレイクの定義の違いがあり、NBAの公式スタッツ記載のファストブレイク割合に4/3をかけて補正しています"
+  ) +
+  scale_x_continuous(labels = percent) +
+  theme_classic()
+plot(p)
+ggsave(plot = p, filename = "./02_output/fast_break_raio_histgram.png", height = 4.5, width = 8, dpi = 100)
+
+# 比率の降順のアウトプット
+hist_base %>%
+  arrange(
+    desc(fast_break_ratio)
+  ) %>%
+  write.csv("./02_output/all_data.csv", fileEncoding = "CP932", row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
