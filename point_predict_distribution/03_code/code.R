e4e276c51f5675a28dd9b4ecac789904dd0e975f
pauze-pauze <yhaimsaaztaakkia1160@gmail.com>
2021-08-13 19:17:35 +0900

    Update code.R

--- point_predict_distribution/03_code/code.R
+++ point_predict_distribution/03_code/code.R
@@ -40,8 +40,8 @@ ft_per <- sum(data_base$FTM) / sum(data_base$FTA)
 game_team_cnt <- nrow(data_base)
 atmpt_num <- (f3ga + f2ga + fta) / game_team_cnt #40分間の平均アテンプト回数
 #本当は延長考慮したいけど悩ましい。分析の対象データから延長を除外すること検討 or もしくはしっかり延長の時間含めて40分間の平均アテンプト出す実際の予測とのずれは延長として捉える。前者の方がいいかも
-exp_num <- 500 #指数分布のnの値
-pts_dist <- NULL #予測された得点の分布
+exp_num <- 250 #指数分布のnの値
+pts_dist <- rep(NA, game_team_cnt) #予測された得点の分布
 
 
 for (i in 1:game_team_cnt){
@@ -116,7 +116,7 @@ for (i in 1:game_team_cnt){
     pts_vec <- append(pts_vec, atmpt_pts * rbinom(n = 1, size = 1, prob = atmpt_per))
   }
   pts <- sum(pts_vec)
-  pts_dist <- append(pts_dist, pts)
+  pts_dist[i] <- pts
 }
 
 pts_dist_tibble <- as_tibble(pts_dist) %>%
@@ -126,6 +126,7 @@ pts_dist_tibble <- as_tibble(pts_dist) %>%
   rename(PTS = value)
 
 
+#可視化
 data_base %>%
   select(PTS) %>%
   mutate(
@@ -137,97 +138,129 @@ data_base %>%
   theme_classic()
 
 
+# 得点の均等な幅で10分割する処理
+quant_actual_pts <- quantile(data_base$PTS, c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9))
+quant_actual_pts <- seq(min(data_base$PTS), max(data_base$PTS), length = 11)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-k <- 1
-atmpt_pts <- atmpt_df %>%
-  filter(rowname == k) %>%
+quant_actual <- data_base %>%
+  select(PTS) %>%
   mutate(
-    pts = case_when(
-      pattern == "f3ga" ~ 3
-      ,pattern == "f2ga" ~ 2
-      ,pattern == "fta" ~ 1
-      ,TRUE ~ 0
+    quantile_10 = case_when(
+      PTS <= quant_actual_pts[2] ~ 1
+      ,PTS <= quant_actual_pts[3] ~ 2
+      ,PTS <= quant_actual_pts[4] ~ 3
+      ,PTS <= quant_actual_pts[5] ~ 4
+      ,PTS <= quant_actual_pts[6] ~ 5
+      ,PTS <= quant_actual_pts[7] ~ 6
+      ,PTS <= quant_actual_pts[8] ~ 7
+      ,PTS <= quant_actual_pts[9] ~ 8
+      ,PTS <= quant_actual_pts[10] ~ 9
+      ,TRUE ~ 10
     )
   ) %>%
-  select(pts) %>%
-  as.numeric()
+  group_by(quantile_10) %>%
+  summarise(cnt = n())
 
-
-5200/60
-
-
-#下記値を最初から足し合わせて、1を超える1個前までのデータをアテンプト回数とする
-sum(rexp(
-  n = 86 # 最大のアテンプト回数。余裕を持った設定にする(1000)
-  , rate = 86　# 40分間の平均のアテンプト回数。この
-))
-
-# パラメータの意味合い把握するやつ
-set.seed(1)
-sum(rexp(
-  n = 86 # 最大のアテンプト回数。余裕を持った設定にする(1000)
-  , rate = 86　# 40分間の平均のアテンプト回数。この
-))
-set.seed(1)
-sum(rexp(
-  n = 86 # 最大のアテンプト回数。余裕を持った設定にする(1000)
-  , rate = 86 / 40　# 1分間の平均のアテンプト回数。この
-)) / 40
-
-f3ga <- 1382
-f2ga <- 1500
-fta <- 1449
-
-as_tibble(rownames_to_column(data.frame(t(rmultinom(n = 100, size = 1, prob = c(f3ga, f2ga, fta))))), "ID")
-
-as_tibble(
-  rownames_to_column(
-    as.data.frame(
-      t(rmultinom(n = 100, size = 1, prob = c(f3ga, f2ga, fta)))
-    )
-  )
-  ,"ID"
-) %>%
+quant_sim <- pts_dist_tibble %>%
+  select(PTS) %>%
   mutate(
-    rowname = as.numeric(rowname)
-    ,f3ga = V1
-    ,f2ga = V2
-    ,fta = V3
-  ) %>%
-  select(
-    -c(V1, V2, V3)
-  ) %>% # 後で縦持ちにして成功率や最終的な成功フラグといい感じのjoinしたほうがいいかも。しなくてもいいかも
-  pivot_longer(
-    -rowname
-    ,names_to = "pattern"
-    ,values_to = "flag"
-  ) %>%
-  filter(flag == 1)
+    quantile_10 = case_when(
+      PTS <= quant_actual_pts[2] ~ 1
+      ,PTS <= quant_actual_pts[3] ~ 2
+      ,PTS <= quant_actual_pts[4] ~ 3
+      ,PTS <= quant_actual_pts[5] ~ 4
+      ,PTS <= quant_actual_pts[6] ~ 5
+      ,PTS <= quant_actual_pts[7] ~ 6
+      ,PTS <= quant_actual_pts[8] ~ 7
+      ,PTS <= quant_actual_pts[9] ~ 8
+      ,PTS <= quant_actual_pts[10] ~ 9
+      ,TRUE ~ 10
+    )
+  )%>%
+  group_by(quantile_10) %>%
+  summarise(cnt = n())
 
+# χ二乗検定で有意差あるか確認
+test_data <- quant_actual %>%
+  left_join(quant_sim, by = "quantile_10") %>%
+  select(-quantile_10) %>%
+  as.matrix()
 
-
-
-
-rbinom(n = 1, size = 1, prob = f3g_per)
-
-
-
+chisq.test(test_data, correct = FALSE)
+# 
+# 
+# 
+# k <- 1
+# atmpt_pts <- atmpt_df %>%
+#   filter(rowname == k) %>%
+#   mutate(
+#     pts = case_when(
+#       pattern == "f3ga" ~ 3
+#       ,pattern == "f2ga" ~ 2
+#       ,pattern == "fta" ~ 1
+#       ,TRUE ~ 0
+#     )
+#   ) %>%
+#   select(pts) %>%
+#   as.numeric()
+# 
+# 
+# 5200/60
+# 
+# 
+# #下記値を最初から足し合わせて、1を超える1個前までのデータをアテンプト回数とする
+# sum(rexp(
+#   n = 86 # 最大のアテンプト回数。余裕を持った設定にする(1000)
+#   , rate = 86　# 40分間の平均のアテンプト回数。この
+# ))
+# 
+# # パラメータの意味合い把握するやつ
+# set.seed(1)
+# sum(rexp(
+#   n = 86 # 最大のアテンプト回数。余裕を持った設定にする(1000)
+#   , rate = 86　# 40分間の平均のアテンプト回数。この
+# ))
+# set.seed(1)
+# sum(rexp(
+#   n = 86 # 最大のアテンプト回数。余裕を持った設定にする(1000)
+#   , rate = 86 / 40　# 1分間の平均のアテンプト回数。この
+# )) / 40
+# 
+# f3ga <- 1382
+# f2ga <- 1500
+# fta <- 1449
+# 
+# as_tibble(rownames_to_column(data.frame(t(rmultinom(n = 100, size = 1, prob = c(f3ga, f2ga, fta))))), "ID")
+# 
+# as_tibble(
+#   rownames_to_column(
+#     as.data.frame(
+#       t(rmultinom(n = 100, size = 1, prob = c(f3ga, f2ga, fta)))
+#     )
+#   )
+#   ,"ID"
+# ) %>%
+#   mutate(
+#     rowname = as.numeric(rowname)
+#     ,f3ga = V1
+#     ,f2ga = V2
+#     ,fta = V3
+#   ) %>%
+#   select(
+#     -c(V1, V2, V3)
+#   ) %>% # 後で縦持ちにして成功率や最終的な成功フラグといい感じのjoinしたほうがいいかも。しなくてもいいかも
+#   pivot_longer(
+#     -rowname
+#     ,names_to = "pattern"
+#     ,values_to = "flag"
+#   ) %>%
+#   filter(flag == 1)
+# 
+# 
+# 
+# 
+# 
+# rbinom(n = 1, size = 1, prob = f3g_per)
+# 
+# 
+# 
